f7e31bdd471b005a6b011ddbd983276e6cc6b917
Phillip Cardenas <mechanicus01@gmail.com>
2025-06-26 14:43:51 -0600

    Initial Commit

---
+++ app-mod2ex1.py
@@ -0,0 +1,8 @@
+# Create a flask app to serve a simple HTML page
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
