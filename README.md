1780782de1ac5a20baa24a28898a89fc939acd1c
Phillip Cardenas <mechanicus01@gmail.com>
2025-06-30 12:24:59 -0600

    Updated README and Employee mgmt html page

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# IBM_HTML
+# IBM HTML, CSS, and JavaScript Projects
 Projects from IBM AI Developer Professional Certificate on Coursera 
+
+# List of Projects from templates folder
+- Flask 
+- HTML5
+
+- CSS
+  - inline
+  - internal
+  - external
+
+- CSS Frameworks- Bootstrap
+  - Bootstrap
+    ![Bootstrap](static/images/bootstrap.png)
+
+  - Tailwind CSS
+    ![Tailwind CSS](static/images/tailwind.png)
+  - Flexbox
+  
+- JavaScript
+  - Employee Management System  
+    ![Employee Management System](static/images/employee_mgmt.png)
+
+
+
--- static/employee_details.js
+++ static/employee_details.js
@@ -1,16 +1,45 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Javascript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
       //... More employee records can be added here
     ];
 
 // Function to display all employees
-const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
         document.getElementById('employeesDetails').innerHTML = totalEmployees;
 
-
+// Calculate total salaries of all employees
 function calculateTotalSalaries() {
       const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
       alert(`Total Salaries: $${totalSalaries}`);
     }        
+
+// display employees details based on department such as the HR department     
+function displayHREmployees() {
+     const hrEmployees = employees.filter(employee => employee.department === 'HR');
+      const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+      document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
+}    
+
+// Find employee by ID and display their details
+function findEmployeeById(employeeId) {
+      const foundEmployee = employees.find(employee => employee.id === employeeId);
+      if (foundEmployee) {
+      document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+      }
+      else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+       }
+   }
+
+// Function to display employees by specialization Javascript
+function jsEmployee(specialty) { // remember too call this function in the HTML file
+      const foundEmployee = employees.find(employee => employee.specialization === specialty);
+      if (foundEmployee) {
+      document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.department} - ${foundEmployee.specialization}</p>`;
+      }
+      else {
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialization';
+       }
+   }
